97731936f99fc950825c203fd4cc221c2c3cc23f
Mario Villasante <mario.villasante@ciencias.unam.mx>
2022-03-19 11:54:18 -0600

    Arreglar Action
    Usé 'black' para reformatear desc_data.py

--- pollos_petrel/desc_data.py
+++ pollos_petrel/desc_data.py
@@ -2,6 +2,4 @@ import pollos_petrel.dummy_model as dm
 
 trainDF = dm.read_training_dataset()
 
-trainDF.describe().to_csv('pollos_petrel/describe_train.csv')
-
-
+trainDF.describe().to_csv("pollos_petrel/describe_train.csv")
