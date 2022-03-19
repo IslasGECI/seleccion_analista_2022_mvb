9af3fb5285418d780a9fc196cf2ad7f37b064a49
Mario Villasante <mario.villasante@ciencias.unam.mx>
2022-03-19 12:44:43 -0600

    Arreglar direcciones: redefinición read_training_data()
    Al importar dummy_model desde la misma carpeta, la dirección del archivo
    train.csv es incorrecta.

--- pollos_petrel/desc_data.py
+++ pollos_petrel/desc_data.py
@@ -1,5 +1,12 @@
-import dummy_model as dm
+import pandas as pd
 
-trainDF = dm.read_training_dataset()
+
+def read_training_dataset() -> pd.DataFrame:
+    training_dataset_path = "train.csv"
+    training_dataset = pd.read_csv(training_dataset_path)
+    return training_dataset
+
+
+trainDF = read_training_dataset()
 
 trainDF.describe().to_csv("describe_train.csv")
