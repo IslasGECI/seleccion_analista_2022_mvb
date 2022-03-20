d691dcac36a6af5d7a08c6a50d1f34ce1f90557c
Mario Villasante <mario.villasante@ciencias.unam.mx>
2022-03-19 20:11:09 -0600

    Elimina mutantes de desc_data.py
    Se eliminan los mutantes generados por mutmut

--- pollos_petrel/desc_data.py
+++ pollos_petrel/desc_data.py
@@ -2,11 +2,7 @@ import pandas as pd
 
 
 def read_training_dataset() -> pd.DataFrame:
-    training_dataset_path = "train.csv"
-    training_dataset = pd.read_csv(training_dataset_path)
-    return training_dataset
+    return pd.read_csv(f"train.csv")
 
 
-trainDF = read_training_dataset()
-
-trainDF.describe().to_csv("describe_train.csv")
+read_training_dataset().describe().to_csv(f"describe_train.csv")
