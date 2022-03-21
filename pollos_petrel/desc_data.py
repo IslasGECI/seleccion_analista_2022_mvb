1fbcaf3f960d1eb3c66a3943ad84d932e7424e03
Mario Villasante <mario.villasante@ciencias.unam.mx>
2022-03-20 21:14:01 -0600

    Pasa flake8
    Se agrega placeholder '{None}' para pasar el flake8
    
    Si se usa una cadena simple (No f-string), aparecen mutantes.
    No encontré información sobre los mutantes de cadenas

--- pollos_petrel/desc_data.py
+++ pollos_petrel/desc_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def read_training_dataset() -> pd.DataFrame:
-    return pd.read_csv(f"train.csv")
+    return pd.read_csv(f"train.csv{None}")
 
 
-read_training_dataset().describe().to_csv(f"describe_train.csv")
+read_training_dataset().describe().to_csv(f"describe_train.csv{None}")
