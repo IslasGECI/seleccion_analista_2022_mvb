f2904b067dd58cce307d1c9136b8c50ff8ea2e94
Mario Villasante <mario.villasante@ciencias.unam.mx>
2022-03-23 18:23:25 -0600

    Arregla estilo del texto
    Se traduce histogramas -> histograms y se cambia data_histogramas()->
    make_histograms()

--- pollos_petrel/make_histograms.py
+++ pollos_petrel/make_histograms.py
@@ -4,11 +4,12 @@ from matplotlib import pyplot as plt
 plt.figure(figsize=(20, 14))  # pragma: no mutate
 
 
-def data_histogramas():
+def make_histograms():
     """Crea histogramas de cada columna de nuestros datos train.csv"""
     data = pd.read_csv("pollos_petrel/train.csv")
     data.hist()
     plt.savefig("histogramas.png")
 
 
-data_histogramas()
+if __name__ == '__main__':
+    make_histograms()
