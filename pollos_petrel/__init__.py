a5e094d39ab2da28b6c7ebc39207d768120f6ffb
Mario Villasante <mario.villasante@ciencias.unam.mx>
2022-03-24 21:07:15 -0600

    Corrige sintaxis # noqa

--- pollos_petrel/__init__.py
+++ pollos_petrel/__init__.py
@@ -5,4 +5,4 @@ from .do_nothing import return_one  # noqa
 from .dummy_model import *  # noqa
 from .describe_data import *  # noqa
 from .make_histograms import *  # noqa
-from .mvb_model import *  #noqa
+from .mvb_model import *  # noqa
