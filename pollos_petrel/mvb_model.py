157240337ecb81adaf436a4d2c0b79ec61f91fa4
Mario Villasante <mario.villasante@ciencias.unam.mx>
2022-03-25 12:42:47 -0600

    Arregla el estilo

--- pollos_petrel/mvb_model.py
+++ pollos_petrel/mvb_model.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from typing import Tuple
 
-def split_data_target(dataset: pd.DataFrame) -> Tuple[pd.DataFrame,pd.DataFrame]:
+
+def split_data_target(dataset: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
     target = dataset[["target"]]
     numeric = dataset.drop(columns=["target", "id"])
     return numeric, target
