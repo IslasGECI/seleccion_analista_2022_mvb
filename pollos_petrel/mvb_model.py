a39c008148a0b62d614575f79e7b5ab991b84958
Mario Villasante <mario.villasante@ciencias.unam.mx>
2022-03-25 20:24:57 -0600

    Agrega segunda función de nuestro modelo
    La función `preprocces_training_data` separa el dataset obtenido de
    `train.csv` en cuatro partes:
    * train_data
    * train_target
    * test_data
    * test_target
    
    La separación en los conjuntos `train_*` y `test_*` es necesaria para
    entrenar el modelo y poder evaluar el error en la predicción.
    (Ya que el archivo `test.csv` no cuenta con una columna `target`)

--- pollos_petrel/mvb_model.py
+++ pollos_petrel/mvb_model.py
@@ -1,3 +1,5 @@
+from sklearn.model_selection import train_test_split
+from pollos_petrel import read_training_dataset
 import pandas as pd
 from typing import Tuple
 
@@ -6,6 +8,18 @@ def split_data_target(dataset: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame
     target = dataset[["target"]]
     numeric = dataset.drop(columns=["target", "id"])
     return numeric, target
+
+
+def preprocces_training_data() -> Tuple[
+    pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame
+]:
+    training_dataset = read_training_dataset()
+    training_dataset = training_dataset.dropna()
+    numeric, target = split_data_target(training_dataset)
+    split_train_data, split_test_data, train_target, test_target = train_test_split(
+        numeric, target
+    )
+    return split_train_data, train_target, split_test_data, test_target
 
 
 def write_mvb_submission():
